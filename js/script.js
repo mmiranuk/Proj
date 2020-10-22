6acdb0a50c54968a7328ec2d1f82273d1261b100
mmiraniuk <mmiranuk@gmail.com>
2020-10-22 14:47:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//"use strict";
+
+// let number = 5;
+// const leftBorderWidth = "1";
+
+// number = 10;
+// console.log(number);
+// alert('hello');
+// const result = confirm('yes');
+// console.log(result);
+
+// const answer = prompt('18?', "18");
+// console.log(typeof(answer));
+const answers = [];
+answers[0] = prompt('Name1', '');
+answers[1] = prompt('SurName', '');
+answers[2] = prompt('Age', '');
+
+document.write(answers);
